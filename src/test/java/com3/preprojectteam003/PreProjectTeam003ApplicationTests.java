6339fc1357056b497f66877d08cc8677d19bd33d
Daeun kim <110896812+DaeunKim9@users.noreply.github.com>
2022-12-20 15:21:05 +0900

    Add files via upload

---
+++ src/test/java/com3/preprojectteam003/PreProjectTeam003ApplicationTests.java
@@ -0,0 +1,13 @@
+package com3.preprojectteam003;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreProjectTeam003ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
