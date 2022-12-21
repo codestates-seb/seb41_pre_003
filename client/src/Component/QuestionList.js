72c37ca4b1762c975e9b41eeef8d6f206af2bf98
himmel37 <dahamong37@gmail.com>
2022-12-21 16:05:09 +0900

    Question List 외 Components 구현 완료

--- client/src/Component/QuestionList.js
+++ client/src/Component/QuestionList.js
@@ -1,2 +1,2 @@
-function QuestionList() {}
+const QuestionList = () => {};
 export default QuestionList;
---
+++ client/src/Pages/Login.js
@@ -0,0 +1,11 @@
+import Header from '../Component/Header';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Home;
