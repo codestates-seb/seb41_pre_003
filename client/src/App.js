b154d0067bef58cdec122652923fd264f2d60219
inaemin <inaemin@gmail.com>
2022-12-21 23:55:12 +0900

    Users page 구현

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './Pages/Home';
+import Users from './Pages/Users';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Home />}></Route>
+          <Route path="/users" element={<Users />}></Route>
         </Routes>
       </Router>
     </div>
