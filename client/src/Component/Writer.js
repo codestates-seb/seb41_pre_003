7e2389d3254523c5415d7635f9b07d2a67fb0436
himmel37 <dahamong37@gmail.com>
2022-12-26 15:20:19 +0900

    question detail page design

---
+++ client/src/Component/Writer.js
@@ -0,0 +1,6 @@
+import '@toast-ui/editor/dist/toastui-editor.css';
+import { Editor } from '@toast-ui/react-editor';
+
+export default function Writer() {
+  return <Editor />;
+}
--- client/src/Pages/QuestionDetail.js
+++ client/src/Pages/QuestionDetail.js
@@ -2,8 +2,8 @@ import Header from '../Component/Header';
 import Nav from '../Component/Nav';
 import Footer from '../Component/Footer';
 import styled from 'styled-components';
-import MidTitle from '../Component/MidTitle';
 import Button from '../Component/Button';
+import Writer from '../Component/Writer';
 
 const QDContainer = styled.div`
   width: 100%;
@@ -15,13 +15,54 @@ const QDContainer = styled.div`
 const QContent = styled.div`
   width: 100%;
   height: auto;
-  border: 1px solid red;
+  margin-bottom: 10px;
+  padding: 10px;
+
+  button {
+    border: none;
+    margin: 10px;
+  }
+`;
+
+const QTitle = styled.div`
+  display: flex;
+  justify-content: space-between;
+  h1 {
+    margin-bottom: 10px;
+  }
+  Button {
+    width: 20%;
+    height: 5%;
+  }
+`;
+
+const QInfo = styled.div`
+  border-bottom: 5px solid var(--light-gray);
+  span {
+    margin: 10px;
+  }
+`;
+
+const Question = styled.div`
+  width: 100%;
+  padding: 10px;
 `;
 
 const AnswerContainer = styled.div`
   width: 100%;
   height: auto;
-  border: 1px solid blue;
+  padding: 10px;
+  h2 {
+    margin: 10px 0 10px 0;
+  }
+`;
+
+const AnswerForm = styled.form`
+  width: 100%;
+  overflow: hidden;
+  Button {
+    margin-top: 10px;
+  }
 `;
 
 const QuestionDetail = () => {
@@ -31,29 +72,37 @@ const QuestionDetail = () => {
       <main>
         <Nav></Nav>
         <QDContainer>
-          <MidTitle title="Question 1" />
-          <Button value="Ask Question"></Button>
           <QContent>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
-            consectetur laoreet elit eu varius. Morbi luctus ex ac luctus
-            ultricies. Nullam tincidunt non erat vel gravida. Pellentesque
-            habitant morbi tristique senectus et netus et malesuada fames ac
-            turpis egestas. Nunc congue nisi eget ante consectetur, at porta
-            massa maximus. Ut consectetur nisi et eros aliquet efficitur sed in
-            mauris. Morbi lobortis tortor quis nibh dignissim, eu eleifend sem
-            ultricies. Aliquam bibendum a lectus a dignissim.
+            <QTitle>
+              <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
+              <Button value="Ask Question"></Button>
+            </QTitle>
+            <QInfo>
+              <span>Asked</span>
+              <span>Modified</span>
+            </QInfo>
+            <Question>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean
+              consectetur laoreet elit eu varius. Morbi luctus ex ac luctus
+              ultricies. Nullam tincidunt non erat vel gravida. Pellentesque
+              habitant morbi tristique senectus et netus et malesuada fames ac
+              turpis egestas. Nunc congue nisi eget ante consectetur, at porta
+              massa maximus. Ut consectetur nisi et eros aliquet efficitur sed
+              in mauris. Morbi lobortis tortor quis nibh dignissim, eu eleifend
+              sem ultricies. Aliquam bibendum a lectus a dignissim.
+            </Question>
+            <button>edit</button>
           </QContent>
-          <button>edit</button>
           <AnswerContainer>
             <p>
               Know someone who can answer? Share a link to this question via
               email, Twitter, or Facebook.
             </p>
             <h2>Your Answer</h2>
-            <form action="/answer/:id" method="post">
-              <textarea cols="92" rows="15"></textarea>
+            <AnswerForm action="/answer" method="post">
+              <Writer></Writer>
               <Button value="Post Your Answer"></Button>
-            </form>
+            </AnswerForm>
           </AnswerContainer>
         </QDContainer>
       </main>
