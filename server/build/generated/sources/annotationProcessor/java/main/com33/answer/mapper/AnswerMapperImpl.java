ca68eb9355b9505aafcd09181a83322fbb35cffa
bbamsooexit <hiaro66@naver.com>
2022-12-27 09:41:20 +0900

    merge jpa

--- server/build/generated/sources/annotationProcessor/java/main/com33/answer/mapper/AnswerMapperImpl.java
+++ server/build/generated/sources/annotationProcessor/java/main/com33/answer/mapper/AnswerMapperImpl.java
@@ -11,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-12-26T21:09:29+0900",
-    comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.1.jar, environment: Java 11.0.17 (Azul Systems, Inc.)"
+    date = "2022-12-26T23:13:35+0900",
+    comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.1.jar, environment: Java 11.0.11 (Oracle Corporation)"
 )
 @Component
 public class AnswerMapperImpl implements AnswerMapper {
