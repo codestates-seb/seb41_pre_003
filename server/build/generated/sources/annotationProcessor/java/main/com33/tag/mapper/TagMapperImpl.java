ce4fae0ea4916be967ba6cd1781426389ed4dde1
bbamsooexit <hiaro66@naver.com>
2023-01-01 18:35:18 +0900

    response header 추가

--- server/build/generated/sources/annotationProcessor/java/main/com33/tag/mapper/TagMapperImpl.java
+++ server/build/generated/sources/annotationProcessor/java/main/com33/tag/mapper/TagMapperImpl.java
@@ -9,8 +9,8 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-01-01T18:10:46+0900",
-    comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.1.jar, environment: Java 11.0.17 (Azul Systems, Inc.)"
+    date = "2023-01-01T18:34:29+0900",
+    comments = "version: 1.4.2.Final, compiler: IncrementalProcessingEnvironment from gradle-language-java-7.5.1.jar, environment: Java 11.0.11 (Oracle Corporation)"
 )
 @Component
 public class TagMapperImpl implements TagMapper {
